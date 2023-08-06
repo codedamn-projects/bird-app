9790a1c0fd0cebbade8c561e497f4b683f8a9b98
Pranav <pranav.techiegeek@gmail.com>
2023-08-06 15:37:40 +0530

    chore: upgrade react & vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-	import.meta.hot.accept()
-}
+root.render(<App />);
